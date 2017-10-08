4ee049c89c55eda3801f04709f1954cb4c700967
ahmedhajjo <ahmad_1996@live.com>
2017-10-08 15:26:27 +0400

    Game111 ClassTest

---
+++ Assets/Collider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider : MonoBehaviour {
+    public Light ll;
+    // Use this for initialization
+      private void OnCollisionEnter(Collision collision)
+      
+        
+
+    {
+        if (collision.gameObject.tag == "Crack")
+        {
+
+            collision.gameObject.GetComponent<MeshRenderer>().material.color = Color.green;
+        
+            }
+
+        if (collision.gameObject.tag == "Platform")
+        {
+            collision.gameObject.GetComponent<MeshRenderer>().material.color = Color.yellow;
+
+
+        }
+       
+            collision.gameObject.GetComponent<MeshRenderer>();
+            ll.color = Color.blue;
+        
+    }
+
+
+    void Start () {
+		
+         
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
